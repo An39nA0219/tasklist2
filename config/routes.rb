641632d0e1a2f2c7ba5a7c080a906afbb7d606c7
EC2 Default User <ec2-user@ip-172-31-43-41.ec2.internal>
2020-09-24 00:34:14 +0000

    already routed

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  get 'tasks', to:'tasks#index'
+  get 'tasks/:id', to:'tasks#show'
+  get 'tasks/new', to:'tasks#new'
+  post 'tasks', to:'tasks#create'
+  get 'tasks/:id/edit', to:'tasks#edit'
+  put 'tasks/:id', to:'tasks#update'
+  destroy 'tasks/:id', to:'tasks#destroy'
+  
+  #コントローラーが複数形なので、複数形にすること
+  #postは新しく作る、putは更新
+  #post,putなどを使えば、URLにcreateとかupdate不要なので省略する
+  #これらを簡単に書くとresources :tasksとなる
+  
 end
