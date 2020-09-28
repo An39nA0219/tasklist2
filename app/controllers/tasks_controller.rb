c12867396bbc1b6c89c2adcf87c974757f5a3b08
EC2 Default User <ec2-user@ip-172-31-43-41.ec2.internal>
2020-09-28 23:35:05 +0000

    all controlled

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,64 @@
 class TasksController < ApplicationController
+  
+  def index
+    @tasks = Task.all
+  end
+  
+  def show
+    @task = Task.find(params[:id])
+  end
+  
+  def new
+    @task = Task.new
+  end
+
+  def create
+    #@taskを保存するためのコード
+    @task = Task.new(task_params)
+    
+    if @task.save
+      flash[:success] = "SUCCESS!"
+      redirect_to @task
+    else
+      flash.now[:danger] = "FAILED"
+      render :new
+    end
+    
+  end
+
+  def edit
+    @task = Task.find(params[:id])
+  end
+
+  def update
+    #@taskを更新するためのコード
+    @task = Task.find(params[:id])
+    
+    if @task.update(task_params)
+      flash[:success] = "SUCCESS!"
+      redirect_to @task
+    else
+      flash.now[:danger] = "FAILED"
+      render :edit
+    end
+  end
+
+  def destroy
+    
+  @task = Task.find(params[:id])
+  @task.destroy
+  
+  flash[:success] = "DELEATED"
+  #redirect_to tasks_pathではだめらしい。
+  #redirectが出てきた場合のみPrefix+_url
+  redirect_to tasks_url
+
+  end
+  
+  private
+  def task_params
+    params.require(:task).permit(:content)
+  end
+
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'tasks#index'
+  resources :tasks
   
-  get 'tasks', to:'tasks#index'
-  get 'tasks/:id', to:'tasks#show'
-  get 'tasks/new', to:'tasks#new'
-  post 'tasks', to:'tasks#create'
-  get 'tasks/:id/edit', to:'tasks#edit'
-  put 'tasks/:id', to:'tasks#update'
-  destroy 'tasks/:id', to:'tasks#destroy'
+=begin
+  
+  get 'tasks/:id', to: 'tasks#show'
+  post 'tasks', to: 'tasks#create'
+  put 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
+  
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks/:id/edit', to: 'tasks#edit'
   
   #コントローラーが複数形なので、複数形にすること
   #postは新しく作る、putは更新
   #post,putなどを使えば、URLにcreateとかupdate不要なので省略する
   #これらを簡単に書くとresources :tasksとなる
-  
+=end
 end
